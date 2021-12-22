d8498b8b2a0eae912b1e177f277c3928cf573c0b
Jesús <jesus.marcos.torero@alumnos.upm.es>
2021-12-23 00:30:34 +0100

    principio de código

--- bluetooth_receiver_esp32/bluetooth_receiver_esp32_DAC/bluetooth_receiver_esp32_DAC.ino
+++ bluetooth_receiver_esp32/bluetooth_receiver_esp32_DAC/bluetooth_receiver_esp32_DAC.ino
@@ -13,7 +13,6 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 // ==> Example to use built in DAC of ESP32
 
 #include "BluetoothA2DPSink.h"
